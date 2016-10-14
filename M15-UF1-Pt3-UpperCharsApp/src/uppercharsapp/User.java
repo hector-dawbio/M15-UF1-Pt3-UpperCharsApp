d2c1959ac65eb579a3ed74c0846636c4b2e71e13
Hector Garcia <garcia.smx1b@gmail.com>
2016-10-14 17:32:08 +0200

    Primer commit

---
+++ M15-UF1-Pt3-UpperCharsApp/src/uppercharsapp/User.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uppercharsapp;
+
+/**
+ * That program converts the vowel to capital letter and consonant to lowercase and count both
+ * @author marc &hector
+ */
+public class User {
+    private String name;
+    //Constructros
+    public User(){}
+    public User(String name){
+        this.name=name;
+    }
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //Methods
+    /**
+     * 
+     * @param name
+     * @return 
+     */
+    public int countlConsonant(String name){
+        return 1;
+    }
+    /**
+     * 
+     * @param name
+     * @return 
+     */
+    public int countlVowel(String name){
+        return 1;
+    }
+    /**
+     * 
+     * @param name to convert
+     * @return name with vowel letters converted to capital letter
+     */
+    public String upperVowels(String name){
+        return "";
+    }
+    /**
+     * This method con
+     * @param name to convert
+     * @return name with consonants converted to lowercase
+     */
+    public String lowerConsonants(String name){
+        return "";
+    }    
+}
