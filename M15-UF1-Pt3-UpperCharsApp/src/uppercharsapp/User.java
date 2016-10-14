6a2552b07a796822395cb0b08246559adeeecb3c
Hector Garcia <garcia.smx1b@gmail.com>
2016-10-14 17:52:00 +0200

    Segundo commit (prueba)

--- M15-UF1-Pt3-UpperCharsApp/src/uppercharsapp/User.java
+++ M15-UF1-Pt3-UpperCharsApp/src/uppercharsapp/User.java
@@ -46,7 +46,7 @@ public class User {
      * @return name with vowel letters converted to capital letter
      */
     public String upperVowels(String name){
-        return "Probando";
+        return "ProbandoProbando";
     }
     /**
      * This method con
